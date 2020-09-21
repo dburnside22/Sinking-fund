3bfbe5522f907a6feea13741fc61a90c5f4e4d6c
Daniel Burnside <daniel.burnside@daveramsey.com>
2020-09-21 15:34:06 -0500

    add AddFundsToBeAllocated comonent

---
+++ src/components/AddFundsToAllocate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./../styles/AddFundsToAllocate.css";
+
+class AddFundsToAllocate extends React.Component {
+    render() {
+        return {
+
+        }
+    }
+}
+
+export default AddFundsToAllocate;
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './../styles/App.css';
+import AddFundsToAllocate from './AddFundsToAllocate';
 import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className="app-container"> 
+
+      </div>
     </div>
   );
 }
