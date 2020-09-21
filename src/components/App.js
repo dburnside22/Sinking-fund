5994f5354d99501dc9b7f5f98988693b658f23a9
Daniel Burnside <daniel.burnside@daveramsey.com>
2020-09-21 15:23:38 -0500

    added header component and structure to the project

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './../styles/App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
