67e94339b9439c8cbf01ecb9d1ab7ba51d09a2b0
Daniel Burnside <daniel.burnside@daveramsey.com>
2020-09-22 16:17:18 -0500

    added basic input to page

--- src/components/AddFundsToAllocate.js
+++ src/components/AddFundsToAllocate.js
@@ -2,10 +2,33 @@ import React from 'react';
 import "./../styles/AddFundsToAllocate.css";
 
 class AddFundsToAllocate extends React.Component {
-    render() {
-        return {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
 
-        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        )
     }
 }
 
--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Header/>
       <div className="app-container"> 
-
+        <AddFundsToAllocate />
       </div>
     </div>
   );
